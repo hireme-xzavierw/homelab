399a015af41be416ea65ffa7eb35df7dcdba20cb
root <root@DJDEBBI.home.arpa>
2025-12-25 19:39:32 -0500

    created robe.sh to prep for a new feature

---
+++ utilities/robe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##############################################################################
+#
+# Quick Reference
+#
+# Setting Variables: NAME=VALUE
+# Reference Variables: ${NAME}
+# Command Substitution: $(command)
+#
+##############################################################################
+
+
+# Variables
+
+# Core Logic
+
+# Error Checking
