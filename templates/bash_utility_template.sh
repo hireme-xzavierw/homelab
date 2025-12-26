bb6ec070c1762b354114dd4ff1ef58d4f3d1ad00
root <root@DJDEBBI.home.arpa>
2025-12-26 00:58:31 -0500

    docs: added help feature to the bash util template

--- templates/bash_utility_template.sh
+++ templates/bash_utility_template.sh
@@ -8,6 +8,7 @@
 # Reference Variables: ${NAME}
 # Command Substitution: $(command)
 # Functions: function_name () { commands }
+# IF statements: if [ condition ]; then #commands fi
 #
 ##############################################################################
 
@@ -15,7 +16,27 @@
 # Variables
 
 # Functions
+print_help() {
+    echo "Usage: $(basename "$0") [OPTION]"
+    echo ""
+    echo "Description Placeholder"
+    echo " "
+    echo "-h, --help       display this help and text"
+    echo 
+    }
+
 
 # Core Logic
 
+## Help Function Call
+while getopts "h" opt; do
+    case $opt in
+	h)
+	    # Call the help function. Exit the script immediately afterwards.
+	    print_help
+	    exit ;;
+    esac
+done
+## Main
+    
 # Error Checking
