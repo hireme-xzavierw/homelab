af62ce36c2a8dcd7c6f5d6f93cf0c4ea61211c91
root <root@DJDEBBI.home.arpa>
2025-12-25 21:09:58 -0500

    feat: begin work on robe.sh

--- utilities/robe.sh
+++ utilities/robe.sh
@@ -13,6 +13,22 @@
 
 # Variables
 
+## Types of commits
+declare -A types=(
+    types["feat"]="Commit which adds a new feature"
+    types["fix"]="Commit which fixes a bug"
+    types["refactor"]="refactored code that neither fixes a bug nor adds a feature, but rewrites/restructers your code"
+    types["chore"]="changes that do not relate to a fix or feature and don't modify src or test files, \"e.g.\", updating dependencies, modifying .gitignore, renaming files or directories."
+    types["feat"]="Commits which add a new feature"
+    types["feat"]="Commits which add a new feature"
+    types["feat"]="Commits which add a new feature"
+    types["feat"]="Commits which add a new feature"
+    types["feat"]="Commits which add a new feature"
+    types["feat"]="Commits which add a new feature"
+    types["feat"]="Commits which add a new feature"
+    types["feat"]="Commits which add a new feature"
+)
+
 # Core Logic
 
 # Error Checking
